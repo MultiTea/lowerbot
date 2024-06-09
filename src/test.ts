323466f46d2adbfecd8679dbf8819abc879b5c64
deeryeen <husky@deeryeen.com>
2024-06-09 17:52:42 +0200

    Modified test command for deploy testing.

--- src/test.ts
+++ src/test.ts
@@ -4,7 +4,7 @@ import { Composer } from "grammY";
 
 export const test = new Composer();
 
-test.command("start", (ctx) => ctx.reply("Welcome! Up and running!"));
+test.command("start", (ctx) => ctx.reply("Welcome! Up and running! My name is lowerbot !"));
 test.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()}`));
 test.command("pong", (ctx) => ctx.reply(`Ping! ${new Date()}`));
 test.command("big", (ctx) => ctx.reply("Bang!"));
